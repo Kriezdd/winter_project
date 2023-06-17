712413f8a965f8bc05a95d2e82d7af88bb18503b
Kriezdd <blagushin_nikita@mail.ru>
2023-06-18 02:55:50 +0300

    project structure changes v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
-import './styles/App.scss';
+import Header from "./components/Header/Header";
 
 function App() {
     return (
