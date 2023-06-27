9f289ec6a8bd63fc1e9a136d0765b4645e0c969f
GROUL46 <tesla499@mail.ru>
2023-06-27 16:58:00 +0300

    fix main_page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "./components/Header/Header";
 import "./App.scss";
 import "./components/Catalog/Catalog.scss";
+import qr from "./assets/mainpage/qr.png";
+import bg from "./assets/mainpage/bg.png";
+
 function App() {
     return (
         <div className="App">
-            <div className="MainPage">
+            <div className="MainPage" style={{backgroundImage: `url(${bg})`}}>
+              <div className="MainContent">
                 <h1>О НАС</h1>
                 <p>
                     KINOSPOT – это сайт, который всегда держит вас в курсе<br/> всех новостей
@@ -17,12 +20,13 @@ function App() {
                 <h2>QR CODE</h2>
                 <p>на тестовый функционал с алгоритмом<br/> по подбору фильмов в телеграмм</p>
                 <div className="QRcode">
-                    <a href="">
-                        <img src=""/>
+                    <a href="https://t.me/KinoSpotBot">
+                        <img src={qr} alt="QR код телеграмм бота"/>
                     </a>
                 </div>
                 <p>Подобрать фильм</p>
             </div>
+          </div>
         </div>
     );
 }
