22435cafb1fc9307d6fe6ec231f5e7f9f69239a2
GROUL46 <tesla499@mail.ru>
2023-06-28 06:55:17 +0300

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import "./Footer.scss";
 import { useNavigate } from "react-router-dom";
-import logo from "./../../assets/footer/logo.png";
+import logo from "./../../assets/footer/logo.svg";
+import iconOpenF from "./../../assets/footer/iconOpenF.svg";
+import bkToTopIcon from "./../../assets/footer/bkToTopIcon.svg";
+import "./Footer"
+
+
+
+const handlerScrollUp = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+    });
+}
+
+
 const Footer = () => {
-  return (
-    <div>
-      <div className="Footer">
-        <img src={logo}/>
-      </div>
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <div>
+            <div className="Footer">
+                <img src={iconOpenF} className="icon"></img>
+                <img src={logo} className="logo" onClick={() => navigate("/")} />
+                <img src={bkToTopIcon} className="back-to-top" onClick={handlerScrollUp} />
+            </div>
+        </div>
+    );
+
 };
 
-export default Footer;
+export default Footer;;
